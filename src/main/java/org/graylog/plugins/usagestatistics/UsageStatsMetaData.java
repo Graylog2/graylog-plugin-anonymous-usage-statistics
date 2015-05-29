3dd4b1530ea0e695f15080a2561e97145e8633b2
Bernd Ahlers <bernd@graylog.com>
2015-05-29 16:30:18 +0200

    Bump plugin version.

--- src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
+++ src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
@@ -24,7 +24,7 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public class UsageStatsMetaData implements PluginMetaData {
-    public static final Version VERSION = new Version(1, 0, 3);
+    public static final Version VERSION = new Version(1, 0, 4);
 
     @Override
     public String getUniqueId() {
