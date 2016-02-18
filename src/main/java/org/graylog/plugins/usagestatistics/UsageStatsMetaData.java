c9815aa33071ae23e91bd4fe12a45f94fc9b4e6e
Bernd Ahlers <bernd@graylog.com>
2016-02-18 19:31:23 +0100

    Bump Graylog dependency and plugin version to 2.0.0-alpha.3

--- src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
+++ src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
@@ -24,7 +24,7 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public class UsageStatsMetaData implements PluginMetaData {
-    public static final Version VERSION = new Version(2, 0, 0, "alpha.2");
+    public static final Version VERSION = new Version(2, 0, 0, "alpha.3");
 
     @Override
     public String getUniqueId() {
