4f7264aafc58ed86c2ed045378dcd42de11a79d1
Bernd Ahlers <bernd@graylog.com>
2016-05-26 16:57:16 +0200

    Bump Graylog Server version to 2.0.2

--- src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
+++ src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
@@ -24,7 +24,7 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public class UsageStatsMetaData implements PluginMetaData {
-    public static final Version VERSION = new Version(2, 0, 1);
+    public static final Version VERSION = new Version(2, 0, 2);
 
     @Override
     public String getUniqueId() {
