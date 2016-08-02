0c4b6b6323f7b8759b10443856cae4f141c2fb78
Edmundo Alvarez <edmundo@graylog.com>
2016-08-02 17:22:14 +0200

    Bumping versions to 2.1.0-beta.2

--- src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
+++ src/main/java/org/graylog/plugins/usagestatistics/UsageStatsMetaData.java
@@ -24,7 +24,7 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public class UsageStatsMetaData implements PluginMetaData {
-    public static final Version VERSION = new Version(2, 1, 0, "beta.1");
+    public static final Version VERSION = new Version(2, 1, 0, "beta.2");
 
     @Override
     public String getUniqueId() {
@@ -64,6 +64,6 @@ public class UsageStatsMetaData implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(2, 1, 0, "beta.1");
+        return new Version(2, 1, 0);
     }
 }
